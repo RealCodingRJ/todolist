158131cea1df111e1f590d9093539a4eafbb681f
RYAN JAMES <realcoder2023@gmail.com>
2025-11-29 00:07:40 -0500

    added getting inpu from input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,71 @@
-export function Page() {
+"use client";
 
-  
-  
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import "./main.css";
+import { useFormState } from "react-dom";
+
+export default function Page() {
+  // const [todo, useTodos] = useState("");
+  const [value, setValue] = useState("");
+  const [value2, setValueTwo] = useState("");
+
+  function setUserInput(event: any) {
+    const values = event.target.value;
+    setValue(values);
+  }
+
+  function setUserInputTwo(event: any) {
+    const values = event.target.value;
+    setValueTwo(values);
+  }
+
+  function getHandled(e: any) {
+    console.log(value);
+    console.log(value2);
+    e.preventDefault();
+  }
+
+  function Button() {
+    return (
+      <div>
+        <button onClick={getHandled}>Submit</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <header>
+        <div>
+          <Link href="/">List Maker</Link>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+              <Link href="/About">About</Link>
+              <Link href="/Projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="container">
+        <main>
+          <div>
+            <input placeholder={"Enter Todo One: "} onChange={setUserInput} />
+          </div>
+          <div>
+            <input
+              placeholder={"Enter Todo One: "}
+              onChange={setUserInputTwo}
+            />
+          </div>
+        </main>
+      </div>
+      <Button />
+    </div>
+  );
 }
